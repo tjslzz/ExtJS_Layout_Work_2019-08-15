37f1fcb81b32528338572cdfab350444528a0a1f
tjslzz <3244188372@qq.com>
2019-08-16 09:51:11 +0800

    [jerry Li] feat : refresh

--- main_src/layout.js
+++ main_src/layout.js
@@ -1,6 +1,5 @@
 //第一题
-let firstQuestion = new Ext.form.FormPanel({
-    xtype: 'fieldset',
+let firstQuestion = new Ext.form.FieldSet({
     id: 'card-0',
     height: 200,
     title: 'Q1 番茄酱属于以下哪种辣鸡',
@@ -20,8 +19,7 @@ let firstQuestion = new Ext.form.FormPanel({
     }]
 })
 //最后一题
-let SecondQuestion = new Ext.form.FormPanel({
-    xtype: 'fieldset',
+let SecondQuestion = new Ext.form.FieldSet({
     id: 'card-1',
     height: 200,
     title: 'Q50 王大叔出门遛狗后，哪些是正确处理狗狗粑粑的办法？（多选）',
@@ -36,24 +34,42 @@ let SecondQuestion = new Ext.form.FormPanel({
         boxLabel: '受污染的报纸浸水后当湿辣鸡处理'
     }]
 })
+// var writer = new Ext.data.JsonWriter({
+//     encode: false
+// });
+// var reader = new Ext.data.JsonReader({idProperty: 'id'}, {name:'info'});
+// var proxy = new Ext.data.HttpProxy({
+//     url: 'text.json'
+// });
+// var store = new Ext.data.Store({
+//     proxy: proxy,
+//     reader: reader,
+//     writer: writer,
+//     autoLoad: true,
+//     autoSave: false  // -- disable cell-updates
+// });
+// var rec = {id:1, name:'foo@bar.com'}
+
 //提交
 let submitConfig = new Ext.form.FormPanel({
     xtype: 'form',
     id: 'card-2',
     labelWidth: 100,
     items: [{
-        xtype:'textfield',
+        xtype: 'textfield',
         fieldLabel: '第一题',
         name: 'first',
         id: 'first',
         allowBlank: false,
+        msgTarget: 'side'
     }, {
-        width:500,
-        xtype:'textarea',
+        width: 500,
+        xtype: 'textarea',
         fieldLabel: '最后一题',
         name: 'last',
         id: 'last',
-        allowBlank: false
+        allowBlank: false,
+        msgTarget: 'side'
     }
     ],
     buttons: [{
@@ -65,7 +81,15 @@ let submitConfig = new Ext.form.FormPanel({
         text: '提交',
         handler: function () {
             if (Ext.getCmp('card-2').getForm().isValid()) {
-                Ext.Msg.alert('Congratulation!', Ext.getCmp('card-2').getForm().getValues(true).replace(/&/g, ', '));
+                let str = ""
+                for(i in Ext.getCmp('card-2').getForm().getFieldValues(true)){
+                    str += `${i} : ${Ext.getCmp('card-2').getForm().getFieldValues(true)[i]} , `
+                }
+                Ext.Msg.alert('Congratulation!', str);
+
+                // Ext.Msg.alert('Congratulation!', `${Ext.getCmp('card-2').getForm().getValues(true)}`);
+                // Ext.Msg.alert('Congratulation!', `${Ext.getCmp('card-2').getForm().items.items[0].getValue()} , ${Ext.getCmp('card-2').getForm().items.items[1].getValue()}`);
+
             }
 
         }
